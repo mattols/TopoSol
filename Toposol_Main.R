e8a2584d88cb442fa76a286238087cdda864b304
mattols <mattolson616@gmail.com>
2020-07-31 23:41:09 -0600

    Added plot examples to Main

--- Toposol_Main.R
+++ Toposol_Main.R
@@ -27,7 +27,6 @@ save_path = NULL
 year = 2016; start_month = 4; end_month = 9
 ###################
 
-
 # CALL FUNCTIONS
 source(paste0(dirpath,"/TopoSol_Functions.R"))
 # GENERATE VARIABLES FOR GLACIER BASIN
@@ -46,3 +45,20 @@ tf <- tf.models(dem0, glacier, months, save_path)
 #   tf[[3]] = Shaded relief (component of topographic shading)
 #   tf[[4]] = Combined (total impact of all topographic attributes)
 # *more info in The Cryosphere publication 
+
+## PLOT THE RESULTS
+# basic plot 1 (combined topography)
+plot(tf[[4]], main="Combined change in direct SW due to topography", col=rev(heat.colors(20)), 
+     legend.args = list(text = bquote(Delta~W~m^-2), side = 1, font = 2, line = -7.1, adj = 0,cex = 0.9))
+plot(glacier,add=T); contour(tf[[5]],add=T); legend("topright","Elevation (m)", lwd=1, cex=0.75, bty='n')
+# basic plot 2 (total shading)
+plot(tf[[2]]+tf[[3]], main="Change in direct SW due to topographic shading", col=rev(blues9), 
+     legend.args = list(text = bquote(Delta~W~m^-2), side = 1, font = 2, line = -7.1, adj = 0,cex = 0.9))
+plot(glacier,add=T); contour(tf[[5]],add=T); legend("topright","Elevation (m)", lwd=1, cex=0.75, bty='n')
+# basic plot 3 (comparison of SW topographic impacts along elevation)
+dft = as.data.frame(tf);dft = dft[complete.cases(dft$DEM),]; cols = c("firebrick", "deepskyblue", "blue","grey10")
+plot(smooth.spline(dft[,4]~dft$DEM), ylab = "", main = "Changes in direct SW due to topography",
+     xlab = "Glacier elevation (m)", col=cols[4],type='l', lwd = 2)
+title(ylab = bquote(Change~'in'~irradiance~Delta~W~m^-2),line=2.2)
+for (i in 1:3){lines(smooth.spline(dft[,i]~dft$DEM), col = cols[i], lwd = 2)}
+abline(h=0,lty=3); legend("topright",names(dft)[1:4], lwd=2, col=cols, cex=0.75, bty='n')
